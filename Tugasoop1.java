c3cc22050956e6663a5992bd7aa75fc920a18058
AisyahKL7 <103208231+AisyahKL7@users.noreply.github.com>
2022-04-07 23:53:45 +0700

    Add files via upload

---
+++ Tugasoop1.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugasoop1;
+
+/**
+ *
+ * @author aisyah
+ */
+
+public class hashira{
+    int hashira;
+    
+  public hashira (String name) {
+    System.out.printIn("Name chosen is:" + name);
+}
+public void setAge(int age) {
+    hashiraAge = age;
+}
+ public int getAge(){
+    System.out.printIn("hashira's Age is: " + hasiraAge);
+    return hashiraAge;
+}
+ public static void main (String []args) {
+     hashira kny = new hashira ( "giyuu" );
+     hashirakny.setAge (19);
+     hashirakny.getAge();
+     System.out.printIn("variable value:" + hashirakny.hashiraAge);
+  }  
+}
